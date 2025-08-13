5aa690c6fad8d2e258988afa846d1bee19049d4e
arleyortega25 <oarleyfernando21@gmail.com>
2025-08-12 20:56:50 -0500

    fixing cors origin

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://deploy-frontend-mern-mongodb.vercel.app/",
     credentials: true,
   })
 );
